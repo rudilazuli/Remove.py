33f3fc94cbdaa362fe7f25331f06c476f596f0db
rudilazuli <100395138+rudilazuli@users.noreply.github.com>
2022-04-23 01:15:33 -0700

    Create ShoppingList.py
    Practicing removing items from list using .remove() function. Two grocery stores are updating their inventory list and remove items they already have in stock or have a duplicate order for.

---
+++ ShoppingList.py
@@ -0,0 +1,12 @@
+# Your code below: 
+order_list = ["Celery", "Orange Juice", "Orange", "Flatbread"]
+print(order_list)
+
+order_list.remove("Flatbread")
+print(order_list)
+
+# Secord store list
+new_store_order_list = ["Orange", "Apple", "Mango", "Broccoli", "Mango"]
+print(new_store_order_list)
+new_store_order_list.remove("Mango")
+print(new_store_order_list)
